92d22ac9c7a50ec1ee198a3f6b0ee5b5bee5c6d1
JONATHAN ROBERTO CALIXTO PESSOA <550773095@alunos.etec.itaqua>
2019-04-11 22:28:43 -0300

    Criando uma nova pagina de cerveja, e excluindo antigas paginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'cerveja-detalhes/:id', loadChildren: './cerveja-detalhes/cerveja-detalhes.module#CervejaDetalhesPageModule' },
+  { path: 'add-cerveja', loadChildren: './add-cerveja/add-cerveja.module#AddCervejaPageModule' },
 ];
 
 @NgModule({
